2331d91e93237ab81a426fedc2b510b6b7e49283
Xavier Loeza <xloeza@nearsoft.com>
2016-11-11 10:25:11 -0700

    Improving any extension method

--- src/array-linq.js
+++ src/array-linq.js
@@ -22,18 +22,15 @@ Object.method('where', function(callback){
 });
 
 Object.method('any', function(callback){    
-    if(typeof callback === "function"){
-        var arrayLength = this.length;
-        for(var x = 0; x < arrayLength; x++ ){
+    var arrayLength = this.length;
+    for(var x = 0; x < arrayLength; x++ ){
+        if(typeof callback === "function"){
             if(callback(this[x])){
                 return true;
             }
         }
-    }
-    else{
-        var arrayLength = this.length;
-        for(var x = 0; x < arrayLength; x++ ){
-            var anyValuesArrayLenght = arguments.length; 
+        else{
+            var anyValuesArrayLenght = arguments.length;
             for(var y = 0; y < anyValuesArrayLenght; y++){
                 if(this[x] === arguments[y]){
                     return true;
@@ -41,7 +38,6 @@ Object.method('any', function(callback){
             }
         }
     }
-    
     return false;
 });
 
